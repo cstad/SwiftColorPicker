808383fd6f3d0d7795a1d5302ffe66a05b88e326
Chris Stadler <christopher.stadler01@gmail.com>
2014-12-25 20:18:29 -0600

    Demonstrate how to use the ColorPicker

--- SwiftColorPicker/ViewController.swift
+++ SwiftColorPicker/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // UIView: ColorPicker reference
+    @IBOutlet var colorPicker: ColorPicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Call setColor on ColorPicker to a user defined color to illustrate how to startup the ColorPicker with a specific color.
+        // The color used here is irrelevant and could be any color; possibly retrieved via stored settings or state of your app.
+        colorPicker.setColor(UIColor(hue: 0.5, saturation: 0.5, brightness: 0.5, alpha: 1.0))
     }
 
     override func didReceiveMemoryWarning() {
